9d8127526283ae00cf8b56d6c4407a9e33233d7f
Abdul Rauf <abdulraufmustakim@gmail.com>
2023-02-05 19:37:10 +0700

    feature/NE-12345: test

--- main.go
+++ main.go
@@ -1,14 +1,53 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
+	"errors"
 	"fmt"
+	"os"
 	"os/exec"
+	"regexp"
 )
 
 func main() {
-	cmd := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD")
+	msg, err := getCommitMsg()
+	if err != nil {
+		panic(err)
+	}
 
+	branch, err := cmdCurrentBranch()
+	if err != nil {
+		panic(err)
+	}
+
+	var stdout, stderr bytes.Buffer
+	cmd := exec.Command("git", "commit", "-m", fmt.Sprintf("%s: %s", branch, msg))
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	if err := cmd.Run(); err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(stdout.String())
+
+}
+
+func getCommitMsg() (string, error) {
+	// get commit message
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter a string: ")
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+
+	return input, nil
+}
+
+func cmdCurrentBranch() (string, error) {
+	// get branch name until ticket number
+	cmd := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD")
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	var stderr bytes.Buffer
@@ -17,9 +56,14 @@ func main() {
 	if err := cmd.Run(); err != nil {
 		fmt.Println("Error:", err)
 		fmt.Println("STDERR:", stderr.String())
-		return
+		return "", err
 	}
 
-	branch := out.String()
-	fmt.Println("Current branch:", branch)
+	re := regexp.MustCompile(`(\w+/\w+-\d+).*`)
+	matches := re.FindStringSubmatch(out.String())
+	if len(matches) > 1 {
+		return matches[1], nil
+	}
+
+	return "", errors.New("unable to get current branch")
 }
